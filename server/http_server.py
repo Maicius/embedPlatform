4b11223073e6d236eba3457fcd8a8b644069aec7
maicius <maicius@outlook.com>
2019-03-27 16:04:57 +0800

    添加多进程启动脚步， start_all.py

---
+++ server/http_server.py
@@ -0,0 +1,7 @@
+import os
+
+def start_http_server():
+    os.system("python3 /Users/maicius/code/embedPlatform/manage.py runserver 8000")
+
+if __name__ =='__main__':
+    start_http_server()
--- start_all.py
+++ start_all.py
@@ -1,18 +1,53 @@
 from server.xmpp_server import start_xmpp_server
 from server.coap_server import start_coap_server
 from server.mqtt_subscribe import start_mqtt_server
-
+from server.http_server import start_http_server
 from client.coap_client import start_coap_client
 from client.http_client import start_http_client
 from client.mqtt_client import start_mqtt_client
+import multiprocessing
 
 
-# 启动客户端
-# start_coap_client()
-start_http_client()
-start_mqtt_client()
+if __name__ == '__main__':
 
-# 启动服务器
-start_coap_server()
-start_xmpp_server()
-start_mqtt_server()
+    http_server = multiprocessing.Process(target=start_http_server)
+    http_server.daemon = True
+
+    http_client = multiprocessing.Process(target=start_http_client)
+    http_client.daemon = True
+
+    mqtt_client = multiprocessing.Process(target=start_mqtt_client)
+    mqtt_client.daemon = True
+
+    coap_client = multiprocessing.Process(target=start_coap_client)
+    coap_client.daemon = True
+
+    # TODO XMPP CLIENT
+
+    coap_server = multiprocessing.Process(target=start_coap_server)
+    coap_server.daemon = True
+
+    xmpp_server = multiprocessing.Process(target=start_xmpp_server)
+    xmpp_server.daemon = True
+
+    mqtt_server = multiprocessing.Process(target=start_mqtt_server)
+    mqtt_server.daemon = True
+
+
+    http_server.start()
+    http_client.start()
+    mqtt_client.start()
+    coap_client.start()
+
+    coap_server.start()
+    xmpp_server.start()
+    mqtt_server.start()
+
+    http_server.join()
+    http_client.join()
+    mqtt_client.join()
+    coap_client.join()
+
+    coap_server.join()
+    xmpp_server.join()
+    mqtt_server.join()
