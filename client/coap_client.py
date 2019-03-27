d7fbf0d729de4b8a65b94a280d874c202780c5c2
wmcai <48835116+wmcai@users.noreply.github.com>
2019-03-27 23:18:53 +0800

    Update coap_client.py

--- client/coap_client.py
+++ client/coap_client.py
@@ -1,6 +1,7 @@
 from embed_nju.util.jedis import pop_raw_data
 from embed_nju.util.constant import RAW_WET_KEY
 from coapthon.client.helperclient import HelperClient
+import time
 
 host = "127.0.0.1"
 port = 5683
@@ -9,6 +10,8 @@ path ="wet"
 client = HelperClient(server=(host, port))
 while True:
     payload=pop_raw_data(RAW_WET_KEY)
-    response = client.post(path,payload)
+    if payload:
+        response = client.post(path,payload)
+        time.sleep(1)
     # print(response.pretty_print())
 client.stop()
