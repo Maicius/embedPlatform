e08ce13c2f2b4bdc9631adef07a6245bd5b5e3e5
奋斗的zpp_star <2106777948@qq.com>
2019-03-27 12:38:11 +0800

    Add files via upload
    发布数据到服务器，（MQTT服务器用的是iot.eclipse.org）

---
+++ Client-publish.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json
+
+
+embed_distance=[{"time":"2019-03-22 14:47:18","value":"76"}]
+embed_light=[{"time":"2019-03-22 14:47:18","value":"76"}]
+embed_temperature=[{"time":"2019-03-22 14:47:18","value":"76"}]
+embed_data=[{"name":"embed_distance","time":"2019-03-22 14:47:18","value":"72"},{"name":"embed_light","time":"2019-03-22 14:47:18","value":"73"},{"name":"embed_temperature","time":"2019-03-22 14:47:18","value":"74"}]
+
+while True:
+    print(json.dumps(embed_data))
+    print("send success")
+    publish.single("paho/temperature",
+                   #payload="this is message:%s" %idx,
+                   payload=json.dumps(embed_data),
+                   hostname="iot.eclipse.org",
+                   client_id="lora1",
+                   # qos = 0,
+                   # tls=tls,
+                   port=1883,
+                   protocol=mqtt.MQTTv311)
