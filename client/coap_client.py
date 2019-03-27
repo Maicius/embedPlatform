64d055b7aa9b4b55c95af8f019c2479d6bf10c95
wmcai <48835116+wmcai@users.noreply.github.com>
2019-03-27 23:14:59 +0800

    coapthon client

--- client/coap_client.py
+++ client/coap_client.py
@@ -1,28 +1,14 @@
-import logging
-import asyncio
-
-from aiocoap import *
 from embed_nju.util.jedis import pop_raw_data
 from embed_nju.util.constant import RAW_WET_KEY
+from coapthon.client.helperclient import HelperClient
 
-logging.basicConfig(level=logging.INFO)
+host = "127.0.0.1"
+port = 5683
+path ="wet"
 
-async def main():
-    protocol = await Context.create_client_context()
+client = HelperClient(server=(host, port))
+while True:
     payload=pop_raw_data(RAW_WET_KEY)
-    request = Message(code='GET', payload=payload.encode(encoding='utf-8'), uri='coap://localhost/wet')
-
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-    else:
-        print('Result: %s\n%r'%(response.code, response.payload))
-
-def start_coap_client():
-    asyncio.get_event_loop().run_until_complete(main())
-
-if __name__ == "__main__":
-    while True:
-        start_coap_client()
+    response = client.post(path,payload)
+    # print(response.pretty_print())
+client.stop()
