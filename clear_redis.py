663eba83e577690026f0c4c45bb55fb756ee89a4
maicius <maicius@outlook.com>
2019-03-28 17:06:11 +0800

    添加清理redis的脚本

---
+++ clear_redis.py
@@ -0,0 +1,17 @@
+from embed_nju.util import constant
+from embed_nju.util.jedis import get_pool
+import redis
+def clear_table():
+    conn = redis.Redis(connection_pool=get_pool())
+    conn.delete(constant.RAW_DISTANCE_KEY)
+    conn.delete(constant.RAW_WET_KEY)
+    conn.delete(constant.RAW_LIGHT_KEY)
+    conn.delete(constant.RAW_TEMPERATURE_KEY)
+
+    conn.delete(constant.TEMPERATURE_KEY)
+    conn.delete(constant.LIGHT_KEY)
+    conn.delete(constant.DISTANCE_KEY)
+    conn.delete(constant.WET_KEY)
+
+if __name__ == '__main__':
+    clear_table()
